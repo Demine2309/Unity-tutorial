e000f343f5aaa3d1bdc1846dbc26be09edee6fc2
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-02-01 00:03:53 +0700

    Demine
    1/2/2024

---
+++ Exercise 3 - Variables Advanced/Exercise 3 - Variables Advanced/Program.cs
@@ -0,0 +1,32 @@
+﻿//A) Bugfixing
+//Look at the following Code and check its output. 2+3 is normally not 23. Pretty strange right...
+//Try to figure out why it happens and fix it
+
+string num1 = "2";
+string num2 = "3";
+string sum = (num1 + num2).ToString();
+Console.WriteLine("A) " + sum);
+
+
+//B) Simple Calculator
+//The Player should be able to input two Values which are multiplied
+//and then the result should be output
+
+Console.WriteLine("Enter first Number");
+int input1 = 0;//TODO: Player should be able to enter a number. Hint: you need to conert it! 
+Console.WriteLine("Enter second Number");
+int input2 = 0;//TODO: Player should be able to enter a number.
+int result = input1 * input2;
+Console.WriteLine("B) " + input1 + "*" + input2 + "=" + result);
+
+
+
+//C) Random Damage
+//Change the Code, so that the players deals
+//between 10 and 15 damage to the enemy
+
+int enemyHp = 100;
+Random rng = new Random();
+int dmg = 10;
+enemyHp -= dmg;
+Console.WriteLine("C) You dealt " + dmg + " to the enemy. It now has " + enemyHp + "hp left.");
---
+++ Exercise2_WorkingWithVariables/Exercise2_WOrkingWithVariables/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+//E) Regions
+//Create a seperate Region for every Exercise in this Script.
+#region MyRegion
+//A) Combining strings
+// Combine the two Variables firstName and lastname and save them into fullName.
+// Hint: Don't forget the blank space between firstname and lastname
+string firstName = "Robert";
+string lastName = "Johansson";
+
+string fullName = firstName + " " + lastName;//TODO
+Console.WriteLine("A) " + fullName);
+
+
+
+
+//B) Simplify Code
+// Try to simplify the following codesnippets
+
+int x = 5;
+x += 2;
+
+int y = 3;
+y++;
+
+int z = 2;
+z *= 3;
+
+
+
+
+//C) Using Braces
+int hp = 100;
+int armor = 3;
+int damage = 10;
+//Damage should be reduced by armor. So in this case the player should only receive 7 damage.
+//However the math in this line is flawed.
+//Try to fix it using Braces ( ).
+hp = hp - (damage - armor);//TODO
+Console.WriteLine("D) new HP: " + hp);
+
+
+
+
+//D) Console.ReadLine()
+//The player should be able to enter any word and the computer should then repeat the word which the player entered.
+
+Console.WriteLine("D) Enter any word");
+string input = Console.ReadLine();//TODO Read from the Console
+Console.WriteLine(input);               //TODO: Print entered word again into the console
+
+#endregion
