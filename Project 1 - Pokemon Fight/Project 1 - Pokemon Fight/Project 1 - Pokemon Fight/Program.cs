d4b4c842b5a0307534b0ab3408eda1bb8fb39889
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-02-01 21:34:12 +0700

    Demine
    1/2/2024

---
+++ Project 1 - Pokemon Fight/Project 1 - Pokemon Fight/Project 1 - Pokemon Fight/Program.cs
@@ -0,0 +1,32 @@
+﻿//1.
+Console.Write("Enter Pokemon1 Name: ");
+string pokemon1 = Console.ReadLine();
+Console.Write("Enter Pokemon2 Name: ");
+string pokemon2 = Console.ReadLine();
+
+//2.
+int hp1 = 100, hp2 = 100;
+
+//3.
+Random random = new Random();
+
+int dmg1, dmg2;
+
+//4.
+dmg1 = random.Next(10, 20);
+dmg2 = random.Next(10, 20);
+Console.WriteLine($"{pokemon1} attacks {pokemon2} and deals {dmg1} damage");
+Console.WriteLine($"{pokemon2} attacks {pokemon1} and deals {dmg2} damage");
+hp1 -= dmg2;
+hp2 -= dmg1;
+Console.WriteLine($"{pokemon1}: {hp1}HP");
+Console.WriteLine($"{pokemon2}: {hp2}HP");
+
+dmg1 = random.Next(10, 20);
+dmg2 = random.Next(10, 20);
+Console.WriteLine($"{pokemon1} attacks {pokemon2} and deals {dmg1} damage");
+Console.WriteLine($"{pokemon2} attacks {pokemon1} and deals {dmg2} damage");
+hp1 -= dmg2;
+hp2 -= dmg1;
+Console.WriteLine($"{pokemon1}: {hp1 - dmg2}HP");
+Console.WriteLine($"{pokemon2}: {hp2 - dmg1}HP");
